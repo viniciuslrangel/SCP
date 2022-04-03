44b8e9bc3b0b53dd4ee691aa496c70c4e5dbede4
Vinicius Rangel <me@viniciusrangel.dev>
2022-04-03 05:24:42 -0300

    Door first implementation & assets

--- Source/SCP/private/gameplay/environment/RoomDoor.cpp
+++ Source/SCP/private/gameplay/environment/RoomDoor.cpp
@@ -1,10 +1,81 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "gameplay/environment/RoomDoor.h"
 
-
-#include "gameplay/environment/RoomDoor.h"
+#include "Net/UnrealNetwork.h"
 
 
 // Sets default values
 ARoomDoor::ARoomDoor()
 {
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	DoorPanel = CreateDefaultSubobject<USceneComponent>(TEXT("DoorPanelRoot"));
+	DoorPanel->SetupAttachment(RootComponent);
+	DoorPanel->SetIsReplicated(true);
+
+	OpenMaxTranslation = 190.0f;
+	OpenTime = 1.5f;
+	bIsOpen = false;
+}
+
+void ARoomDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OpeningTimeline = NewObject<UTimelineComponent>(this, FName("OpeningTimeline"));
+	// OpeningTimeline->SetNetAddressable();
+	// OpeningTimeline->SetIsReplicated(true);
+	OpeningTimeline->SetLooping(false);
+	OpeningTimeline->SetTimelineLength(OpenTime);
+	OpeningTimeline->SetTimelineLengthMode(ETimelineLengthMode::TL_TimelineLength);
+	OpeningTimeline->SetPlaybackPosition(0.0f, false);
+	if (OpeningCurve)
+	{
+		FOnTimelineFloat TimelineCallback;
+		TimelineCallback.BindDynamic(this, &ARoomDoor::OnTimelineCallback);
+		OpeningTimeline->AddInterpFloat(OpeningCurve, TimelineCallback);
+	}
+	OpeningTimeline->RegisterComponent();
+}
+
+void ARoomDoor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
+
+void ARoomDoor::OnUse(ASCPCharacter* Subject, UPrimitiveComponent* Comp)
+{
+	if (!Comp) { return; }
+
+	if (!Comp->ComponentHasTag(TEXT("BUTTON")))
+	{
+		return;
+	}
+
+	if (bIsOpen)
+	{
+		OpeningTimeline->Reverse();
+	}
+	else
+	{
+		OpeningTimeline->Play();
+	}
+	bIsOpen = !bIsOpen;
+}
+
+void ARoomDoor::OnHover(ASCPCharacter* Subject, UPrimitiveComponent* Comp, bool bIsStartHover)
+{
+	if (!Comp) { return; }
+
+	if (!Comp->ComponentHasTag(TEXT("BUTTON")))
+	{
+		return;
+	}
+	IInteractive::OnHover(Subject, Comp, bIsStartHover);
+}
+
+void ARoomDoor::OnTimelineCallback(float Val)
+{
+	DoorPanel->SetRelativeLocation({Val * OpenMaxTranslation, 0.0f, 0.0f});
 }
--- Source/SCP/public/gameplay/environment/RoomDoor.h
+++ Source/SCP/public/gameplay/environment/RoomDoor.h
@@ -1,17 +1,48 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
 #include "GameFramework/Actor.h"
+#include "interactive/Interactive.h"
 #include "RoomDoor.generated.h"
 
 UCLASS()
-class SCP_API ARoomDoor : public AActor
+class SCP_API ARoomDoor : public AActor, public IInteractive
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Room Door")
+	USceneComponent* DoorPanel;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Room Door")
+	UCurveFloat* OpeningCurve;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Room Door")
+	float OpenMaxTranslation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Room Door")
+	float OpenTime;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Room Door")
+	bool bIsOpen;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Room Door")
+	UTimelineComponent* OpeningTimeline;
 
 public:
 	// Sets default values for this actor's properties
 	ARoomDoor();
+
+	void BeginPlay() override;
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void OnUse(ASCPCharacter* Subject, UPrimitiveComponent* Comp) override;
+
+	void OnHover(ASCPCharacter* Subject, UPrimitiveComponent* Comp, bool bIsStartHover) override;
+
+private:
+	UFUNCTION()
+	void OnTimelineCallback(float Val);
 };
